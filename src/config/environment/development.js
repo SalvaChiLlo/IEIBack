b852caa8ac0793a4b1efedeadb1ce1c9c02e4706
SalvaChLl <SalvaChLl@gmail.com>
2021-12-01 23:11:47 +0100

    preparacion proyecto

--- src/config/environment/development.js
+++ src/config/environment/development.js
@@ -2,12 +2,20 @@ require('dotenv').config();
 
 module.exports = {
   sequelize: {
-    username: 'root',
-    password: 'adminadmin',
-    database: 'sequelize_database_development',
-    host: 'omv.local',
-    dialect: 'mysql',
+    username: 'ymemqqbelumkfo',
+    password: '9fdbe8d2e45f3cfa51555ef524150d205b67c1247266829b05cd626b4da8be85',
+    database: 'd7v59pbqs3ddap',
+    host: 'ec2-54-195-195-81.eu-west-1.compute.amazonaws.com',
+    dialect: 'postgres',
     logging: console.log,
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+
   },
   seedDB: false,
 };
