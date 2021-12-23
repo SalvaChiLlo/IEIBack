59fc881fba78a532a6dd44259e0a39797b6fb7ab
SalvaChLl <salvachll@gmail.com>
2021-12-24 00:18:34 +0100

    Vaciado de la BD

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,10 @@ const provincia = require('./api/provincia');
 const populateEUS = require('./api/populateEUS');
 const populateCAT = require('./api/populateCAT');
 const populateCV = require('./api/populateCV');
+const path = require('path');
+const db = require(path.join(__dirname, './sqldb'))
+
+
 
 module.exports = (app) => {
   app.use('/api/bibliotecas', biblioteca);
@@ -17,4 +21,23 @@ module.exports = (app) => {
   app.get('/', (req, res) => {
     res.status(200).send('<h1>Server is running</h1>');
   });
+
+  app.delete('/api/drop', async (req, res) => {
+    try {
+      console.log('BORRANDO');
+      await db.sequelize.query(`
+      delete from "Biblioteca" b; delete from "Localidad" l; delete from "Provincia" p;
+      `)
+
+      res.status(200).send('');
+    } catch (error) {
+      handleCatch(error)
+    }
+  });
+
+  function handleCatch(error) {
+    console.log('--------------------------------------------------------------------------')
+    console.error(error)
+    process.exit(1)
+  }
 };
